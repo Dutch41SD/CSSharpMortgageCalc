0307df9ce858bd00e496a8497f8e4b502cf8fde6
Dutch41SD <81736427+Dutch41SD@users.noreply.github.com>
2023-02-28 23:25:44 -0600

    Add project files.

---
+++ CSSharpMortgageCalc/Models/LoanPayment.cs
@@ -0,0 +1,14 @@
+﻿namespace CSSharpMortgageCalc.Models
+{
+    public class LoanPayment
+    {
+        public int Month { get; set; }
+
+        public decimal Payment { get; set; }
+
+        public decimal MonthlyPricipal { get; set; }
+        public decimal MonthlyInterest { get; set; }
+        public decimal TotalInterest { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
